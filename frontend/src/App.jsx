8d1135138469a667d4887849be95b48e7e888bd5
Karim-Said1 <karimsaidsalama1@gmail.com>
2025-05-14 22:12:36 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate, Link } from "react-router-dom";
-import Navbar from './components/Navbar';
-import Dashboard from "./pages/Dashboard";
-import DriversLocations from "./pages/DriversLocations";
-import RetailersLocations from "./pages/RetailersLocations";
-import CBMCalculator from "./pages/CBMCalculator";
-import TripAssignment from "./pages/TripAssignment";
-import SupplierTripAssignment from "./pages/SupplierTripAssignment";
+import Navbar from './src/components/Navbar';
+import Dashboard from "./src/pages/Dashboard";
+import DriversLocations from "./src/pages/DriversLocations";
+import RetailersLocations from "./src/pages/RetailersLocations";
+import CBMCalculator from "./src/pages/CBMCalculator";
+import TripAssignment from "./src/pages/TripAssignment";
+import SupplierTripAssignment from "./src/pages/SupplierTripAssignment";
 
 function App() {
   return (
